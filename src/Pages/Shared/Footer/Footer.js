0d9a01aeccb138d8a601308eb0d906959495cd07
Proloycb <proloychacroborty@gmail.com>
2022-04-17 22:05:44 +0600

    notfound page setup

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <div className='bg-black text-gray-100 text-center p-8'>
+        <div className='bg-slate-700 text-gray-100 text-center p-8'>
             <p><small>Copyright &copy; {year}</small></p>
         </div>
     );
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import notfound from '../../../images/notfound.jpg';
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='mx-auto w-1/2'>
+            <h1 className='text-6xl text-center text-orange-500 mt-2 mb-3'>Page not found</h1>
+            <img src={notfound} alt="" />
         </div>
     );
 };
