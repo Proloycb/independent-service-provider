516e89a1c48648fcaf46a3fbf7938e9dc8ab3b9e
Proloycb <proloychacroborty@gmail.com>
2022-04-17 22:28:28 +0600

    loading setup

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -47,7 +47,7 @@ const Register = () => {
                     <input ref={nameRef} className='w-full shadow appearance-none border rounded py-2 px-3 text-gray-700  focus:outline-none focus:shadow-outline mb-4' type="text" name="name" id="" placeholder='Your Name' />
                     <input ref={emailRef} className='w-full shadow appearance-none border rounded py-2 px-3 text-gray-700  focus:outline-none focus:shadow-outline mb-4' type="email" name="email" id="" placeholder='Your Email' required />
                     <input ref={passwordRef} className='w-full shadow appearance-none border rounded py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline mb-4' type="password" name="password" id="" placeholder='Password' required />
-                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-20 block w-75 mx-auto rounded mb-3'>Register</button>
+                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-20 block w-3/4 mx-auto rounded mb-3'>Register</button>
                     <p>{errorElement}</p>
                     <p className='text-slate-700'>Already Have an account?<Link to='/login' className='text-blue-500 decoration-none'>Please Login</Link></p>
                 </form>
--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Loading = () => {
     return (
         <div>
-            <h1>Loading...</h1>
+            <div class="flex justify-center items-center">
+                <div class="spinner-border animate-spin inline-block w-8 h-8 border-4 border-indigo-500 rounded-full" role="status">
+                </div>
+            </div>
         </div>
     );
 };
